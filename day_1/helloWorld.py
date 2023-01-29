8b6f3eaf70c8a3be4f44cee9ddcf8bb7324c9f55
zeerau <zeerabaidu@gmail.com>
2023-01-29 10:14:37 +0100

    fist time commit in 2023

---
+++ day_1/helloWorld.py
@@ -0,0 +1,56 @@
+print('hello world')
+# Day 1 - 30DaysOfPython Challenge
+
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+
+# write string on the python intereactive shell
+print("zarau bello baidu")
+print("baidu jega")
+print("nigeria")
+# Checking data types
+print(type(10))         
+print(type(9.8)) 
+print(type(3.14))       
+print(type(4 -4j))     
+print(type(['Asabeneh', 'Python', 'Finland']))  
+print(type("zarau bello baidu"))
+print(type("baidu jega"))
+print(type("nigeria"))
+
+# number data type example 7,8,9
+#float type example 7.8,5.8 
+# complex type example 6-g,8+s
+# string type example "Hello World",i am a string"
+# boolean type example true,false
+#list type example ["Hello", "World, 9, 8.3]
+"""dict type example {'age': 24,
+'name': 'zarau', 
+'gender': 'female', 
+'marital_status': True,
+ 'skills': ['python', 'javascript', 'html', 'css']
+  } """
+# tuple type example ('inteesar','ukasha','maryam','zahra' )
+#sets type example {True, 5, 'string'}
+
+
+
+# euclidean distance solution
+""" 
+d = √((x2-x1)2 + (y2-y1)2)
+Step by step procedure:
+Find the difference between coordinates:
+(x2-x1) = (10 - 2) = 8
+(y2-y1) = (8 - 3) = 5
+Square the results and sum them up:
+(8)2 + (5)2 = 64 + 25 = 89
+Now Find the square root and that's your result:
+Exact solution: √89 = √89
+Approximate solution: 9.434
+"""
---
+++ day_2/variables.py
@@ -0,0 +1,53 @@
+
+# Day 2: 30 Days of python programming.
+
+
+# Variables in Python
+first_name = 'zarau'
+last_name = 'baidu'
+full_name = 'zarau bello baidu'
+country = 'Nigeria'
+city = 'jega'
+age = 28
+year = 2022
+is_married = True
+is_true = False
+is_light_on = True
+ 
+
+first_name, last_name, country, age, is_married = 'zarau', 'baidu', 'Nigeria', 28, True
+   # Printing the values stored in the variables
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+print(first_name > last_name)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one- num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 
+
+
+
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)

827415df342a6789b0858b4c27340705f099c69a
zeerau <zeerabaidu@gmail.com>
2023-01-30 17:48:13 +0100

    monday commit

---
+++ day_3/operator.py
@@ -0,0 +1,30 @@
+# day 3 exercises
+
+age = 28
+height = 5.5
+complex_value = 4j
+
+base_of_triangle = float(input("Enter base: "))
+height_of_tringle = float(input("Enter height: "))
+area_of_triangle = 0.5 * base_of_triangle * height_of_tringle
+
+print(area_of_triangle)
+
+side_a = float(input("Enter side A: "))
+side_b = float(input("Enter side B: "))
+side_c = float(input("Enter side C: "))
+
+perimeter = side_a + side_b + side_c
+
+print("The perimeter of triangle is", perimeter)
+
+length_of_rectangle =float(input("Enter length of triangle: "))
+width_of_rectangle = float(input("Enter width of triangle: "))
+
+area_of_rectangle = length_of_rectangle * width_of_rectangle
+perimeter_of_rectangle = 2 * (length_of_rectangle + width_of_rectangle)
+
+
+print("The area of rectangle is", area_of_rectangle)
+print("The perimeter of rectangle is",perimeter_of_rectangle)
+

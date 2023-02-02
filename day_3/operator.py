9a1e15129c6dd496a37da43cef62b569b427453f
zeerau <zeerabaidu@gmail.com>
2023-02-02 23:04:57 +0100

    day 3 commit

--- day_3/operator.py
+++ day_3/operator.py
@@ -1,30 +1,73 @@
 # day 3 exercises
 
-age = 28
-height = 5.5
-complex_value = 4j
+# age = 28
+# height = 5.5
+# complex_value = 4j
 
-base_of_triangle = float(input("Enter base: "))
-height_of_tringle = float(input("Enter height: "))
-area_of_triangle = 0.5 * base_of_triangle * height_of_tringle
+# base_of_triangle = float(input("Enter base: "))
+# height_of_tringle = float(input("Enter height: "))
+# area_of_triangle = 0.5 * base_of_triangle * height_of_tringle
 
-print(area_of_triangle)
+# print(area_of_triangle)
 
-side_a = float(input("Enter side A: "))
-side_b = float(input("Enter side B: "))
-side_c = float(input("Enter side C: "))
+# side_a = float(input("Enter side A: "))
+# side_b = float(input("Enter side B: "))
+# side_c = float(input("Enter side C: "))
 
-perimeter = side_a + side_b + side_c
+# perimeter = side_a + side_b + side_c
 
-print("The perimeter of triangle is", perimeter)
+# print("The perimeter of triangle is", perimeter)
 
-length_of_rectangle =float(input("Enter length of triangle: "))
-width_of_rectangle = float(input("Enter width of triangle: "))
+# length_of_rectangle =float(input("Enter length of triangle: "))
+# width_of_rectangle = float(input("Enter width of triangle: "))
 
-area_of_rectangle = length_of_rectangle * width_of_rectangle
-perimeter_of_rectangle = 2 * (length_of_rectangle + width_of_rectangle)
+# area_of_rectangle = length_of_rectangle * width_of_rectangle
+# perimeter_of_rectangle = 2 * (length_of_rectangle + width_of_rectangle)
 
 
-print("The area of rectangle is", area_of_rectangle)
-print("The perimeter of rectangle is",perimeter_of_rectangle)
+# print("The area of rectangle is", area_of_rectangle)
+# print("The perimeter of rectangle is",perimeter_of_rectangle)
 
+# radius = float(input("Enter radius: "))
+# pi = 3.14
+# area_of_circle = pi * radius * radius
+# circumference_of_circle = 2 * pi * radius
+
+
+# print("The area of circle is", area_of_circle)
+# print("the circumference of circle is",circumference_of_circle)
+
+
+# print(len("python")!= len("dragon"))
+
+# print("on" in "python" and "dragon")
+
+# print("jargon" in "I hope this course is not full of jargon.")
+
+# print("on" not in "python" and "dragon")
+
+# text = len("python")
+
+# print(float(str(text)))
+
+# you can check if number is even or odd using the modules operator
+# even_or_odd = 10 % 2
+
+# print(even_or_odd)
+
+# 40
+
+
+# hour = float(input("Enter hours : "))
+# rate = float(input("Enter rate : "))
+
+# print(hour * rate)
+
+y = float(input("Enter years : "))
+
+d = y * 365
+h = d * 24
+m = h * 60
+s = m * 60
+
+print(s)

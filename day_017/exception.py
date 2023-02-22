82cd359a9c406a9b59e640d78f65c651dd31f962
zeerau <zeerabaidu@gmail.com>
2023-02-22 08:55:18 +0100

    exception done

---
+++ day_017/exception.py
@@ -0,0 +1,6 @@
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+nordic_countries = names[:5]
+
+
+es = names[-2] # Estonia is the second to last element in the list
+ru = names[-1] # Russia is the last element in the list
